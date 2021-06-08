9eabe266d8e268863d9a41148dd34118a72e76a3
lg519 <lg519@ic.ac.uk>
2021-06-08 22:50:50 +0200

    obstacle avoidance path finding

---
+++ Obstacle_Avoidance/get_to_destination.py
@@ -0,0 +1,57 @@
+from grid import*
+import collections
+
+class Queue:
+    def __init__(self):
+        self.elements = collections.deque()
+    
+    def empty(self):
+        return not self.elements
+    
+    def put(self, x):
+        self.elements.append(x)
+    
+    def get(self):
+        return self.elements.popleft()
+
+
+def breadth_first_search(graph, start, goal):
+    frontier = Queue()
+    frontier.put(start)
+    came_from = {}
+    came_from[start] = None
+    
+    while not frontier.empty():
+        current = frontier.get()
+        
+        if current == goal:
+            break
+        
+        for next in graph.neighbors(current):
+            if next not in came_from:
+                frontier.put(next)
+                came_from[next] = current
+    
+    return came_from
+
+g = SquareGrid(30, 15)
+DIAGRAM1_WALLS = [from_id_width(node, width=30) for node in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
+g.walls = DIAGRAM1_WALLS
+
+start = (8, 7)
+goal = (17, 2)
+parents = breadth_first_search(g, start, goal)
+
+draw_grid(g, point_to=parents, start=start, goal=goal)
+
+def reconstruct_path(came_from, start, goal):
+    current = goal
+    path = []
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    path.append(start) # optional
+    path.reverse() # optional
+    return path
+
+draw_grid(g, path=reconstruct_path(parents, start=start, goal=goal))
--- Obstacle_Avoidance/grid.py
+++ Obstacle_Avoidance/grid.py
@@ -1,19 +1,4 @@
-import collections
-
-class Queue:
-    def __init__(self):
-        self.elements = collections.deque()
-    
-    def empty(self):
-        return not self.elements
-    
-    def put(self, x):
-        self.elements.append(x)
-    
-    def get(self):
-        return self.elements.popleft()
-
-class Map:
+class SquareGrid:
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -37,10 +22,13 @@ class Map:
         result = filter(self.passable, result)
         return result
 
-
-# utility functions for dealing with square grids
-def from_id_width(node, width):
-    return (node % width, node // width)
+class GridWithWeights(SquareGrid):
+    def __init__(self, width: int, height: int):
+        super().__init__(width, height)
+        self.weights = {}
+    
+    def cost(self, from_node, to_node) -> float:
+        return self.weights.get(to_node, 1)
 
 def draw_tile(graph, node, style):
     r = " . "
@@ -66,28 +54,11 @@ def draw_grid(graph, **style):
         print()
     print("~~~" * graph.width)
 
+def from_id_width(node, width):
+    return (node % width, node // width)
 
-DIAGRAM1_WALLS = [from_id_width(node, width=30) for node in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
 
-
-def breadth_first_search(graph, start):
-    frontier = Queue()
-    frontier.put(start)
-    came_from = {}
-    came_from[start] = None
-    
-    while not frontier.empty():
-        current = frontier.get()
-        for next in graph.neighbors(current):
-            if next not in came_from:
-                frontier.put(next)
-                came_from[next] = current
-    
-    return came_from
-
-g = Map(30, 15)
-g.walls = DIAGRAM1_WALLS
-
-start = (8, 7)
-parents = breadth_first_search(g, start)
-draw_grid(g, point_to=parents, start=start)
+# DIAGRAM1_WALLS = [from_id_width(node, width=30) for node in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
+# g = SquareGrid(30, 15)
+# g.walls = DIAGRAM1_WALLS # long list, [(21, 0), (21, 2), ...]
+# draw_grid(g)
